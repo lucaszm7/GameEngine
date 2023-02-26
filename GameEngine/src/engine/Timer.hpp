c7cfa245ffb00837301f81c1628d5bb7c68a177f
Lucas Morais <lucas.morais@visagio.com>
2023-02-26 19:17:47 -0300

    feat: Timer

---
+++ GameEngine/src/engine/Timer.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <chrono>
+struct Timer
+{
+	using Clock = std::chrono::high_resolution_clock;
+	std::chrono::steady_clock::time_point start;
+	std::chrono::duration<double> duration;
+
+	Timer()
+	{
+		start = Clock::now();
+	}
+
+	void Stop()
+	{
+		duration = Clock::now() - start;
+	}
+
+	inline double ResultMs() const
+	{
+		return duration.count() * 1000.0;
+	}
+
+	double Now()
+	{
+		duration = Clock::now() - start;
+		return duration.count();
+	}
+
+	double NowMs()
+	{
+		duration = Clock::now() - start;
+		return duration.count() * 1000.0;
+	}
+
+	void Reset()
+	{
+		start = Clock::now();
+		duration = std::chrono::duration<double>();
+	}
+};
+	
+inline std::ostream& operator << (std::ostream& out, const Timer& t)
+{
+	out << t.ResultMs() << " ms";
+	return out;
+}
