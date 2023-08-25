f06105d24aca2e92ae8c640583507b6df3f02799
Lucas Morais <lucas.morais446@hotmail.com>
2023-08-25 19:04:02 -0300

    refac: FrameBuffer

--- GameEngine/src/engine/FrameBuffer.hpp
+++ GameEngine/src/engine/FrameBuffer.hpp
@@ -17,13 +17,13 @@ private:
 	std::shared_ptr<unsigned int> pScreenWidth;
 	std::shared_ptr<unsigned int> pScreenHeight;
 
-	std::unique_ptr<Shader> viewportShader;
+	std::unique_ptr<Shader> m_FramebufferShader;
 
-	unsigned int framebuffer;
-    std::unique_ptr<Texture> textureColorBuffer;
+	unsigned int m_MultisampleFramebuffer;
+    std::unique_ptr<Texture> m_MultisampleTextureColorBuffer;
 
-    unsigned int finalFrameBuffer;
-    std::unique_ptr<Texture> finalTextureColorBuffer;
+    unsigned int m_SingleSampleFramebuffer;
+    std::unique_ptr<Texture> m_SingleSampleTextureColorBuffer;
 
     VertexArray quadVAO;
     VertexBufferLayout quadVBL;
@@ -33,7 +33,7 @@ public:
 	FrameBuffer(std::shared_ptr<unsigned int> screenWidth, std::shared_ptr<unsigned int> screenHeight, FrameBuffer::Type type)
 		:pScreenWidth(screenWidth), pScreenHeight(screenHeight), m_Type(type)
 	{
-		viewportShader = std::make_unique<Shader>("resources/shaders/viewport_vertex.shader", "resources/shaders/viewport_fragment.shader");
+		m_FramebufferShader = std::make_unique<Shader>("resources/shaders/viewport_vertex.shader", "resources/shaders/viewport_fragment.shader");
         
         float quadVertices[] = {
             // positions   // texCoords
@@ -58,7 +58,7 @@ public:
 
     FrameBuffer()
     {
-        viewportShader = std::make_unique<Shader>("resources/shaders/viewport_vertex.shader", "resources/shaders/viewport_fragment.shader");
+        m_FramebufferShader = std::make_unique<Shader>("resources/shaders/viewport_vertex.shader", "resources/shaders/viewport_fragment.shader");
 
         m_Type = Type::MONOSAMPLE;
 
@@ -84,27 +84,27 @@ public:
     void OnRenderTexture(const Texture& tex) const
     {
         glDisable(GL_DEPTH_TEST);
-        viewportShader->Bind();
+        m_FramebufferShader->Bind();
         quadVAO.Bind();
         tex.Bind();
         tex.SetFiltering();
-        viewportShader->SetUniform1i("screenTexture", 0);
+        m_FramebufferShader->SetUniform1i("screenTexture", 0);
         glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
         glDrawArrays(GL_TRIANGLES, 0, 6);
         quadVAO.Unbind();
     }
 
     // Render the frame buffer inside a texture
-	void OnRender()
+	void OnRenderDirectlyToScreen()
 	{
         glBindFramebuffer(GL_FRAMEBUFFER, 0); // back to default
 
         // Draw to default frameBuffer e.g. the screen itself
         glClear(GL_COLOR_BUFFER_BIT);
-        viewportShader->Bind();
+        m_FramebufferShader->Bind();
         quadVAO.Bind();
         glDisable(GL_DEPTH_TEST);
-        glBindTexture(GL_TEXTURE_2D, textureColorBuffer->GetID());
+        glBindTexture(GL_TEXTURE_2D, m_MultisampleTextureColorBuffer->GetID());
         glDrawArrays(GL_TRIANGLES, 0, 6);
         quadVAO.Unbind();
 	}
@@ -116,8 +116,8 @@ public:
         {
             // This resolve the multisample texture into a normal texture that we can read from
             // Draw on final image
-            glBindFramebuffer(GL_READ_FRAMEBUFFER, framebuffer);
-            glBindFramebuffer(GL_DRAW_FRAMEBUFFER, finalFrameBuffer);
+            glBindFramebuffer(GL_READ_FRAMEBUFFER, m_MultisampleFramebuffer);
+            glBindFramebuffer(GL_DRAW_FRAMEBUFFER, m_SingleSampleFramebuffer);
             glBlitFramebuffer(0, 0, *pScreenWidth, *pScreenHeight, 0, 0, *pScreenWidth, *pScreenHeight, GL_COLOR_BUFFER_BIT, GL_NEAREST);
         }
 
@@ -129,7 +129,7 @@ public:
         // Get final Image
         ImVec2 wsize = ImGui::GetWindowSize();
         // Because I use the texture from OpenGL, I need to invert the V from the UV.
-        ImGui::Image((ImTextureID)finalTextureColorBuffer->GetID(), wsize, ImVec2(0, 1), ImVec2(1, 0));
+        ImGui::Image((ImTextureID)m_SingleSampleTextureColorBuffer->GetID(), wsize, ImVec2(0, 1), ImVec2(1, 0));
 
         if ((float)*pScreenWidth != ImGui::GetWindowWidth() || (float)*pScreenHeight != ImGui::GetWindowHeight())
         {
@@ -145,7 +145,7 @@ public:
     // Witch frame buffer it will draw
     void Bind() const
     {
-        glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+        glBindFramebuffer(GL_FRAMEBUFFER, m_Type == Type::MULTISAMPLE ? m_MultisampleFramebuffer : m_SingleSampleFramebuffer);
     }
 
     void Unbind() const
@@ -156,41 +156,50 @@ public:
     // Called when changed viewport size
     void Reset()
     {
-        glDeleteFramebuffers(1, &framebuffer);
+        glDeleteFramebuffers(1, &m_SingleSampleFramebuffer);
+        
+        if (m_Type == Type::MULTISAMPLE)
+            glDeleteFramebuffers(1, &m_MultisampleFramebuffer);
+
         _Init();
     }
 
 private:
     void _Init()
     {
-        // framebuffer configuration
-        // -------------------------
+        // Final Image
+        m_SingleSampleFramebuffer = 0;
+        glGenFramebuffers(1, &m_SingleSampleFramebuffer);
+        glBindFramebuffer(GL_FRAMEBUFFER, m_SingleSampleFramebuffer);
 
-        // Create FrameBuffer
-        framebuffer = 0;
-        glGenFramebuffers(1, &framebuffer);
-        glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+        // Create Attachment (memory) to write the color on
+        m_SingleSampleTextureColorBuffer = std::make_unique<Texture>(*pScreenWidth, *pScreenHeight, Texture::Type::RAW);
+        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_SingleSampleTextureColorBuffer->GetID(), 0);
 
-        if (m_Type == Type::MONOSAMPLE)
+        // Create a renderbuffer object for depth and stencil attachment (we won't be sampling these)
+        unsigned int final_rbo;
+        glGenRenderbuffers(1, &final_rbo);
+        glBindRenderbuffer(GL_RENDERBUFFER, final_rbo);
+        glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, *pScreenWidth, *pScreenHeight); // use a single renderbuffer object for both a depth AND stencil buffer.
+        glBindRenderbuffer(GL_RENDERBUFFER, 0);
+        glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, final_rbo); // now actually attach it
+
+        // now that we actually created the framebuffer and added all attachments we want to check if it is actually complete now
+        if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+            std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << std::endl;
+
+        Unbind();
+
+        if (m_Type == Type::MULTISAMPLE)
         {
-            // Create Attachment (memory) to write the color on
-            textureColorBuffer = std::make_unique<Texture>(*pScreenWidth, *pScreenHeight, Texture::Type::RAW);
-            glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureColorBuffer->GetID(), 0);
+            // Create FrameBuffer
+            m_MultisampleFramebuffer = 0;
+            glGenFramebuffers(1, &m_MultisampleFramebuffer);
+            glBindFramebuffer(GL_FRAMEBUFFER, m_MultisampleFramebuffer);
 
-            // Create a renderbuffer object for depth and stencil attachment (we won't be sampling these)
-            unsigned int rbo;
-            glGenRenderbuffers(1, &rbo);
-            glBindRenderbuffer(GL_RENDERBUFFER, rbo);
-            glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, *pScreenWidth, *pScreenHeight); // use a single renderbuffer object for both a depth AND stencil buffer.
-            glBindRenderbuffer(GL_RENDERBUFFER, 0);
-            glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo); // now actually attach it
-        }
-
-        else if (m_Type == Type::MULTISAMPLE)
-        {
             // Create Attachment (memory) to write the color on
-            textureColorBuffer = std::make_unique<Texture>(*pScreenWidth, *pScreenHeight, Texture::Type::MULTISAMPLED);
-            glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D_MULTISAMPLE, textureColorBuffer->GetID(), 0);
+            m_MultisampleTextureColorBuffer = std::make_unique<Texture>(*pScreenWidth, *pScreenHeight, Texture::Type::MULTISAMPLED);
+            glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D_MULTISAMPLE, m_MultisampleTextureColorBuffer->GetID(), 0);
 
             // Create a renderbuffer object for depth and stencil attachment (we won't be sampling these)
             unsigned int rbo;
@@ -205,31 +214,6 @@ private:
                 std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << std::endl;
 
             Unbind();
-
-            {
-                // Final Image
-                finalFrameBuffer = 0;
-                glGenFramebuffers(1, &finalFrameBuffer);
-                glBindFramebuffer(GL_FRAMEBUFFER, finalFrameBuffer);
-
-                // Create Attachment (memory) to write the color on
-                finalTextureColorBuffer = std::make_unique<Texture>(*pScreenWidth, *pScreenHeight, Texture::Type::RAW);
-                glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, finalTextureColorBuffer->GetID(), 0);
-
-                // Create a renderbuffer object for depth and stencil attachment (we won't be sampling these)
-                unsigned int final_rbo;
-                glGenRenderbuffers(1, &final_rbo);
-                glBindRenderbuffer(GL_RENDERBUFFER, final_rbo);
-                glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, *pScreenWidth, *pScreenHeight); // use a single renderbuffer object for both a depth AND stencil buffer.
-                glBindRenderbuffer(GL_RENDERBUFFER, 0);
-                glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, final_rbo); // now actually attach it
-                
-                // now that we actually created the framebuffer and added all attachments we want to check if it is actually complete now
-                if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
-                    std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << std::endl;
-                
-                Unbind();
-            }
         }
     }
 
