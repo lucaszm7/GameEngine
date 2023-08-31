7d4debaf35c2c12d6baf0303c2b1b4057fb68614
Lucas Morais <lucas.morais446@hotmail.com>
2023-08-31 10:52:44 -0300

    feat: shadowMap

--- GameEngine/src/engine/ShadowMap.cpp
+++ GameEngine/src/engine/ShadowMap.cpp
@@ -1,8 +1,12 @@
 #include "ShadowMap.h"
 
-ShadowMap::ShadowMap()
-	:fboId(0), texId(0)
+ShadowMap::ShadowMap(std::shared_ptr<unsigned int> screenWidth, std::shared_ptr<unsigned int> screenHeight)
+	:fboId(0), texId(0),
+	shader  ("../resources/shader/ShadowMap/shadowMap_vertex.shader",      "../resources/shader/ShadowMap/shadowMap_fragment.shader")
 {
+	pScreenWidth = screenWidth;
+	pScreenHeight = screenHeight;
+
 	glGenFramebuffers(1, &fboId);
 
 	glGenTextures(1, &texId);
@@ -20,14 +24,36 @@ ShadowMap::ShadowMap()
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
 
-void ShadowMap::OnUpdate()
+void ShadowMap::OnUpdate(const glm::vec3& lightPosition, const std::vector<Model>& objects)
 {
+
 	glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
 	glBindFramebuffer(GL_FRAMEBUFFER, fboId);
 	glClear(GL_DEPTH_BUFFER_BIT);
+	shader.Bind();
 	
-	/*ConfigureShaderAndMatrices();
-	RenderScene();*/
-	
-	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	float near_plane = 1.0f, far_plane = 7.5f;
+	glm::mat4 lightProjection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, near_plane, far_plane);
+	glm::mat4 lightView = glm::lookAt(glm::vec3(-2.0f, 4.0f, -1.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+	lightSpaceMatrix = lightProjection * lightView;
+
+	shader.SetUniformMatrix4fv("lightSpaceMatrix", lightSpaceMatrix);
+
+	for (const auto& obj : objects)
+	{
+		obj.Draw(shader);
+	}
+
+	glBindFramebuffer(GL_FRAMEBUFFER, 2);
+	glViewport(0, 0, *pScreenWidth, *pScreenHeight);
+}
+
+void ShadowMap::Bind(unsigned int slot)
+{
+	glActiveTexture(GL_TEXTURE0 + slot);
+	glBindTexture(GL_TEXTURE_2D, texId);
+}
+
+void ShadowMap::DebugShadowMap() const
+{
 }
--- GameEngine/src/engine/ShadowMap.h
+++ GameEngine/src/engine/ShadowMap.h
@@ -1,6 +1,12 @@
 #pragma once
 
 #include <GL/glew.h>
+#include <memory>
+#include <glm/glm.hpp>
+
+#include "model.h"
+#include "shader.h"
+#include "FrameBuffer.hpp"
 
 class ShadowMap
 {
@@ -10,7 +16,17 @@ class ShadowMap
 	inline static const unsigned int SHADOW_WIDTH = 1024;
 	inline static const unsigned int SHADOW_HEIGHT = 1024;
 
-	ShadowMap();
-	void OnUpdate();
+	inline static std::shared_ptr<unsigned int> pScreenWidth;
+	inline static std::shared_ptr<unsigned int> pScreenHeight;
+
+	Shader shader;
+
+public:
+	glm::mat4 lightSpaceMatrix;
+
+	ShadowMap(std::shared_ptr<unsigned int> screenWidth, std::shared_ptr<unsigned int> screenHeight);
+	void OnUpdate(const glm::vec3& lightPosition, const std::vector<Model>& objects);
+	void Bind(unsigned int slot);
+	void DebugShadowMap() const;
 };
 
